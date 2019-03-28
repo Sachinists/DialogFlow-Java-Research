b4f986f6a8395fd72d446ee0597677f655e19b5a
Debjyoti Pandit <debjyoti.pandit@ad.infosys.com>
2019-03-28 15:20:04 +0530

    initial hello world

---
+++ src/main/java/com/example/dialogflow/api/DialogFlowAPI.java
@@ -0,0 +1,13 @@
+package com.example.dialogflow.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DialogFlowAPI {
+
+	@GetMapping
+	public String helloWorld() {
+		return "You have successfully deployed your first Hello World Spring Boot Application";
+	}
+}
---
+++ src/main/java/com/example/dialogflow/beans/FulfillmentResponse.java
@@ -0,0 +1,5 @@
+package com.example.dialogflow.beans;
+
+public class FulfillmentResponse {
+
+}
